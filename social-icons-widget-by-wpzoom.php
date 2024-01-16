8c669a16150cff3d7de77298348de43d3b3e730c
Alexei Samarschi <samarschi@gmail.com>
2024-01-16 11:17:33 +0200

    Removed php files and js for pointers
    removed the potential vulnerability in the plugin

--- social-icons-widget-by-wpzoom.php
+++ social-icons-widget-by-wpzoom.php
@@ -32,7 +32,6 @@ if ( ! defined( 'WPZOOM_SOCIAL_ICONS_PLUGIN_BASE' ) ) {
 }
 
 require_once plugin_dir_path( __FILE__ ) . 'includes/classes/class-wpzoom-social-icons-settings.php';
-require_once plugin_dir_path( __FILE__ ) . 'includes/zoom-helper.php';
 
 $wpzoom_social_icons_settings = WPZOOM_Social_Icons_Settings::get_settings();
 
