99b683c9a7ee5706e1dea785af8f7f3d369006a5
Pavel Ciorici <cioricip@gmail.com>
2026-01-26 19:18:03 +0100

    .

--- block/src/social-sharing-block/components/Inspector.js
+++ block/src/social-sharing-block/components/Inspector.js
@@ -336,7 +336,7 @@ export default function Inspector({ attributes, setAttributes }) {
 							}}>Pro</span>
 						</>
 					}
-					initialOpen={false}
+					initialOpen={true}
 				>
 					<div className="wpzoom-pro-features-upsell">
 						<ToggleControl
@@ -372,31 +372,57 @@ export default function Inspector({ attributes, setAttributes }) {
 							</p>
 							<div style={{
 								display: 'flex',
-								gap: '8px',
-								opacity: '0.5',
-								pointerEvents: 'none'
+								gap: '6px',
+								flexWrap: 'wrap'
 							}}>
 								<span style={{
+									display: 'inline-flex',
+									alignItems: 'center',
+									gap: '5px',
 									padding: '6px 10px',
-									background: '#f0f0f0',
-									borderRadius: '4px',
-									fontSize: '12px',
-									color: '#666'
-								}}>ChatGPT</span>
+									background: '#10A37F',
+									borderRadius: '50px',
+									fontSize: '11px',
+									color: '#fff',
+									fontWeight: '500'
+								}}>
+									<svg width="14" height="14" viewBox="0 0 24 24" fill="#fff">
+										<path d="M22.282 9.821a5.985 5.985 0 0 0-.516-4.91 6.046 6.046 0 0 0-6.51-2.9A6.065 6.065 0 0 0 4.981 4.18a5.985 5.985 0 0 0-3.998 2.9 6.046 6.046 0 0 0 .743 7.097 5.98 5.98 0 0 0 .51 4.911 6.051 6.051 0 0 0 6.515 2.9A5.985 5.985 0 0 0 13.26 24a6.056 6.056 0 0 0 5.772-4.206 5.99 5.99 0 0 0 3.997-2.9 6.056 6.056 0 0 0-.747-7.073zM13.26 22.43a4.476 4.476 0 0 1-2.876-1.04l.141-.081 4.779-2.758a.795.795 0 0 0 .392-.681v-6.737l2.02 1.168a.071.071 0 0 1 .038.052v5.583a4.504 4.504 0 0 1-4.494 4.494zM3.6 18.304a4.47 4.47 0 0 1-.535-3.014l.142.085 4.783 2.759a.771.771 0 0 0 .78 0l5.843-3.369v2.332a.08.08 0 0 1-.033.062L9.74 19.95a4.5 4.5 0 0 1-6.14-1.646zM2.34 7.896a4.485 4.485 0 0 1 2.366-1.973V11.6a.766.766 0 0 0 .388.676l5.815 3.355-2.02 1.168a.076.076 0 0 1-.071 0l-4.83-2.786A4.504 4.504 0 0 1 2.34 7.872zm16.597 3.855l-5.833-3.387L15.119 7.2a.076.076 0 0 1 .071 0l4.83 2.791a4.494 4.494 0 0 1-.676 8.105v-5.678a.79.79 0 0 0-.407-.667zm2.01-3.023l-.141-.085-4.774-2.782a.776.776 0 0 0-.785 0L9.409 9.23V6.897a.066.066 0 0 1 .028-.061l4.83-2.787a4.5 4.5 0 0 1 6.68 4.66zm-12.64 4.135l-2.02-1.164a.08.08 0 0 1-.038-.057V6.075a4.5 4.5 0 0 1 7.375-3.453l-.142.08L8.704 5.46a.795.795 0 0 0-.393.681zm1.097-2.365l2.602-1.5 2.607 1.5v2.999l-2.597 1.5-2.607-1.5z"/>
+									</svg>
+									ChatGPT
+								</span>
 								<span style={{
+									display: 'inline-flex',
+									alignItems: 'center',
+									gap: '5px',
 									padding: '6px 10px',
-									background: '#f0f0f0',
-									borderRadius: '4px',
-									fontSize: '12px',
-									color: '#666'
-								}}>Claude</span>
+									background: '#D97706',
+									borderRadius: '50px',
+									fontSize: '11px',
+									color: '#fff',
+									fontWeight: '500'
+								}}>
+									<svg width="14" height="14" viewBox="0 0 24 24" fill="#fff">
+										<path d="M4.709 15.955l4.72-2.647.08-.08v-.08l-.08-.08-2.085-1.19-.08-.08h-.08l-2.538 1.43c-.795.476-1.272 1.35-1.272 2.25.08.954.557 1.748 1.335 2.17zm8.478 5.483c.716.397 1.59.397 2.306 0l7.035-3.916c.716-.397 1.192-1.19 1.192-2.012V7.672c0-.874-.476-1.668-1.192-2.012l-7.035-3.996c-.716-.397-1.59-.397-2.306 0L6.152 5.66c-.716.397-1.192 1.19-1.192 2.012v3.44l4.8 2.726.08.08h.08l2.085-1.19.08-.08v-.08l-.08-.08-4.72-2.647V7.75l.08-.08 4.8-2.726h.08l4.8 2.726.08.08v7.76l-.08.08-4.8 2.726h-.08l-2.538-1.43-.08-.08h-.08l-2.085 1.19-.08.08v.08l.08.08z"/>
+									</svg>
+									Claude
+								</span>
 								<span style={{
+									display: 'inline-flex',
+									alignItems: 'center',
+									gap: '5px',
 									padding: '6px 10px',
-									background: '#f0f0f0',
-									borderRadius: '4px',
-									fontSize: '12px',
-									color: '#666'
-								}}>Perplexity</span>
+									background: '#20808D',
+									borderRadius: '50px',
+									fontSize: '11px',
+									color: '#fff',
+									fontWeight: '500'
+								}}>
+									<svg width="14" height="14" viewBox="0 0 24 24" fill="#fff">
+										<path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 2.4c5.302 0 9.6 4.298 9.6 9.6 0 5.302-4.298 9.6-9.6 9.6-5.302 0-9.6-4.298-9.6-9.6 0-5.302 4.298-9.6 9.6-9.6zm0 2.4a7.2 7.2 0 1 0 0 14.4 7.2 7.2 0 0 0 0-14.4zm0 2.4a4.8 4.8 0 1 1 0 9.6 4.8 4.8 0 0 1 0-9.6zm0 2.4a2.4 2.4 0 1 0 0 4.8 2.4 2.4 0 0 0 0-4.8z"/>
+									</svg>
+									Perplexity
+								</span>
 							</div>
 						</div>
 
